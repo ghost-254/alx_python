e81b8107c1c065f36d0b045947bff69abf1fbca0
ghost-254 <nevilleotieno6@gmail.com>
2023-08-01 06:02:59 -0700

    printing numbers from 0 to 99 in two digits

--- python-hello_world/5-print_comb2.py
+++ python-hello_world/5-print_comb2.py
@@ -1,3 +1,3 @@
 # A program that prints numbers from 0 to 99 in 2 digits.
 for a in range(0, 100):
-    print("{:02d}".format(a), end=", ")
+    print("{:02d}".format(a), end=", " if a < 99 else "")
