1caa4d763196a409ac88d8e3418d6d141317b639
ghost-254 <nevilleotieno6@gmail.com>
2023-08-05 03:48:17 -0700

    bb

--- python-import_modules/0-add.py
+++
@@ -1,6 +0,0 @@
-# A program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3
-from add_0 import add
-a = 1
-b = 2
-result = add(a, b)
-print(f"{a} + {b} = {result}")
