0e005583314fca803694cf71429dda4818a2f6ea
ghost-254 <nevilleotieno6@gmail.com>
2023-08-05 04:27:55 -0700

    0. Import a simple function from a simple file

---
+++ python-import_modules/0-add.py
@@ -0,0 +1,11 @@
+# A program that imports the function def add(a, b): from the file add_0.py and prints the result of the addition 1 + 2 = 3
+exec(open("add_0.py").read())
+
+def main():
+    a = 1
+    b = 2
+    result = add(a, b)
+    print("{} + {} = {}".format(a, b, result))
+
+if __name__ == "__main__":
+    main()
---
+++ python-import_modules/add_0.py
@@ -0,0 +1,3 @@
+# The function def add(a, b): Adds the sum of two numbers a and b
+def add(a, b):
+    return (a + b)
