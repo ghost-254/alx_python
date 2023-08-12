ccadc5751530ce81a46507854c24af93fc7d1c4e
ghost-254 <nevilleotieno6@gmail.com>
2023-08-12 08:09:40 -0700

    4. Raise exception

--- python-import_modules/4-raise_exception.py
+++ python-import_modules/4-raise_exception.py
@@ -5,4 +5,4 @@ def raise_exception():
         # We then attempt to treat value as an integer
         result = value + 1
     except TypeError:
-        print("TypeError: Cannot perform addition on string and integer.")
+        print("Exception has been raised")
