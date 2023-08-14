eef221a4776087c44898595ad4a90a70a9c09f23
ghost-254 <nevilleotieno6@gmail.com>
2023-08-14 03:19:42 -0700

    2. Update dictionary

--- python-more_data_structures/2-update_dictionary.py
+++ python-more_data_structures/2-update_dictionary.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
     a_dictionary[key] = value
+
+def print_sorted_dictionary(a_dictionary):
+    keys = sorted(a_dictionary.keys())
+    for key in keys:
+        print(f"{key}: {a_dictionary[key]}")
