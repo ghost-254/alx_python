bc28f580e99917cc9364df4f0002646eee175233
ghost-254 <nevilleotieno6@gmail.com>
2023-08-16 04:48:25 -0700

    3. Geometry module

---
+++ python-inheritance/3-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+"""
+3-base_geometry.py
+A module that defines an empty BaseGeometry class for representing a base geometry.
+"""
+class BaseGeometry:
+    """
+    An empty class representing a base geometry.
+    """
+    pass
