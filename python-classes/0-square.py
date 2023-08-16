e2fd11b531000cbaf04efb744ca6172ae7a0b054
ghost-254 <nevilleotieno6@gmail.com>
2023-08-16 03:12:47 -0700

    0. Square with size

--- python-classes/0-square.py
+++ python-classes/0-square.py
@@ -3,5 +3,14 @@ class Square:
     def __init__(self, size):
         self.__size = size
     
+    def get_size(self):
+        return self.__size
+    
+    def set_size(self, new_size):
+        if new_size >= 0:
+            self.__size = new_size
+        else:
+            raise ValueError("Size cannot be negative")
+    
     def __str__(self):
         return f"Square with side length {self.__size}"
