44beec3860aaad93a501ba750fc22ab0adad1773
ghost-254 <nevilleotieno6@gmail.com>
2023-08-16 05:11:30 -0700

    4. Improve Geometry

---
+++ python-inheritance/4-base_geometry.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+"""
+4-base_geometry.py
+A module that defines the BaseGeometry class for representing a base geometry.
+"""
+
+class BaseGeometry:
+    """
+    A class representing a base geometry.
+
+    Methods:
+        area(self): Raises an Exception with the message "area() is not implemented".
+    """
+
+    def area(self):
+        """
+        Raises an Exception with the message "area() is not implemented".
+        """
+        raise Exception("area() is not implemented")
