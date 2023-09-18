a3cd12b0734370256a35c80b4bc97869a113bd78
ghost-254 <nevilleotieno6@gmail.com>
2023-09-18 05:12:35 -0700

    6. First state model

---
+++ python-object_relational_mapping/model_state.py
@@ -0,0 +1,30 @@
+#!/usr/bin/python3
+import sys
+from sqlalchemy import create_engine, Column, Integer, String
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+
+# Create a declarative base instance
+Base = declarative_base()
+
+# Define the State class
+class State(Base):
+    __tablename__ = 'states'
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
+
+if __name__ == "__main__":
+    # Check for the correct number of command-line arguments
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>"
+              .format(sys.argv[0]))
+        sys.exit(1)
+
+    # Create the database engine
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]), pool_pre_ping=True)
+
+    # Create the table in the database
+    Base.metadata.create_all(engine)
+
+    print("Table 'states' created successfully.")
