d0e6f3e7cf29590f7b36649135040bafb2a5d73d
ghost-254 <nevilleotieno6@gmail.com>
2023-09-18 04:26:33 -0700

    updates

--- python-object_relational_mapping/2-my_filter_states.py
+++ python-object_relational_mapping/2-my_filter_states.py
@@ -5,7 +5,8 @@ if __name__ == "__main__":
     # Check if the correct number of command-line arguments is provided
     if len(sys.argv) != 5:
         print(
-            "Usage: python filter_states.py <username> <password> <database> <state_name>"
+            "Usage: python filter_states.py <username> <password>"
+            "<database> <state_name>"
         )
         sys.exit(1)
 
@@ -28,8 +29,11 @@ if __name__ == "__main__":
     # Create a cursor object
     cursor = db.cursor()
 
-    # Execute the SQL query to fetch states where the name matches the provided state_name
-    query = "SELECT * FROM states WHERE UPPER(name) = UPPER(%s) ORDER BY id ASC"
+    # SQL-query to fetch states where the name matches the provided state_name
+    query = (
+        "SELECT * FROM states WHERE name = %s OR BINARY name = %s "
+        "ORDER BY id ASC"
+    )
     cursor.execute(query, (state_name,))
 
     # Fetch all rows from the result set
