c3728065491566ff352b84e40b808f0b54665f20
ghost-254 <nevilleotieno6@gmail.com>
2023-09-19 02:44:25 -0700

    Task: 5.Number templates for the route

---
+++ python-web_framework/5-number_template.py
@@ -0,0 +1,42 @@
+#!/usr/bin/python3
+"""
+This module defines a Flask web application with six routes.
+"""
+
+from flask import Flask, render_template
+
+app = Flask(__name__)
+
+@app.route('/', strict_slashes=False)
+def hello_hbnb():
+    """Displays 'Hello HBNB!' when accessing the root URL."""
+    return "Hello HBNB!"
+
+@app.route('/hbnb', strict_slashes=False)
+def hbnb():
+    """Displays 'HBNB' when accessing the /hbnb route."""
+    return "HBNB"
+
+@app.route('/c/<text>', strict_slashes=False)
+def c_text(text):
+    """Displays 'C ' followed by the value of the 'text' variable."""
+    return "C " + text.replace("_", " ")
+
+@app.route('/python/', defaults={'text': 'is cool'}, strict_slashes=False)
+@app.route('/python/<text>', strict_slashes=False)
+def python_text(text):
+    """Displays 'Python ' followed by the value of the 'text' variable."""
+    return "Python " + text.replace("_", " ")
+
+@app.route('/number/<int:n>', strict_slashes=False)
+def is_number(n):
+    """Displays 'n is a number' only if 'n' is an integer."""
+    return "{} is a number".format(n)
+
+@app.route('/number_template/<int:n>', strict_slashes=False)
+def number_template(n):
+    """Renders an HTML page with 'Number: n' inside the BODY tag."""
+    return render_template('5-number.html', n=n)
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=5000)
